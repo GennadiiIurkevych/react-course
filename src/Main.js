20b635ef0572b254bf338f62f1bfd7fc7017249f
GennadiiIurkevych <yurgensport@gmail.com>
2022-06-04 16:41:07 +0300

    change here

--- src/Main.js
+++ src/Main.js
@@ -1,69 +1,23 @@
+function ItemMarks  (mark)  {
+  return (
+    <li>
+      <div class="choice">
+        <input type="radio" className="scales" name="scales" />
+        <label for="scales" className="label">{mark.index}</label>
+      </div>
+    </li>
+  )
+}
 
 function Main () {
+  const createArray = (size) => [...new Array (size)]
   return (
     <div className="main">
       <h2>How would you rate our course?</h2>
       <ul>
-        <li>
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">1</label>
-          </div>
-        </li>
-        <li>         
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">2</label>
-          </div>
-        </li>
-        <li>
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">3</label>
-          </div>
-        </li>
-        <li>         
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">4</label>
-          </div>
-        </li>
-        <li>
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">5</label>
-          </div>
-        </li>
-        <li>         
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">6</label>
-          </div>
-        </li>
-        <li>
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">7</label>
-          </div>
-        </li>
-        <li>         
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">8</label>
-          </div>
-        </li>
-        <li>
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">9</label>
-          </div>
-        </li>
-        <li>         
-          <div class="choice">
-            <input type="radio" className="scales" name="scales" />
-            <label for="scales" className="label">10</label>
-          </div>
-        </li>
+        {
+          createArray(10).map((el, index) => <ItemMarks index={index +1}></ItemMarks>)
+        }
       </ul>
     </div> 
   );
